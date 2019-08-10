7efb8914b7ede15903cdd5d738c31e4faf731e2c
Kasidit <33252458+topfine2541@users.noreply.github.com>
2019-08-10 23:38:49 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+//$message = $_REQUEST[‘message’];
+$name_buy = $_REQUEST[‘name_buy’];
+$mobile = $_REQUEST[‘mobile’];
+$fruit = $_REQUEST[‘fruit’];
+$number = $_REQUEST[‘number’];
+$address = $_REQUEST[‘address’];
+$message = “\n”.”order by = “.$name_buy.”\n”.” mobile “.$mobile.”\n”.” fruit order = 
+“.$fruit.”\n”.”number = “.$number.”\n”.” Address “.$address.”\n”;
+$chOne = curl_init();
+curl_setopt( $chOne, CURLOPT_URL, “https://notify-api.line.me/api/notify");
+// SSL USE
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0);
+//POST
+curl_setopt( $chOne, CURLOPT_POST, 1);
+// Message
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, $message);
+//ถ้าต้องการใส่รุป ให้ใส่ 2 parameter imageThumbnail และimageFullsize
+curl_setopt( $chOne, CURLOPT_POSTFIELDS, “message=$message”);
+// follow redirects
+curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1);
+//ADD header array
+$headers = array( ‘Content-type: application/x-www-form-urlencoded’, ‘Authorization: 
+Bearer nrh2URXmUrhs5BpbBFbNrptCqK1Rarhzh2Zfq5MMJPh’, );
+curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
+//RETURN
+curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec( $chOne );
+//Check error
+if(curl_error($chOne)) { echo ‘error:’ . curl_error($chOne); }
+else { $result_ = json_decode($result, true);
+echo “status : “.$result_[‘status’]; echo “message : “. $result_[‘message’]; }
+//Close connect
+curl_close( $chOne );
+?>
